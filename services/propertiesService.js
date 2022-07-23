8f93aa36b85a440d4e9e3e93d4c65af70b5459a6
unknown <juanesrojas@ingenieros.com>
2022-07-22 23:09:20 -0500

    fixed the properties service to lookup info from other collections

--- services/propertiesService.js
+++ services/propertiesService.js
@@ -7,7 +7,51 @@ const {ObjectId} = moongoose.Types;
 const properties = async (filter) =>{
     try{
         const query = filterMapping (filter);
-        const checkProperties = await PropertyModel.find(query);
+        //console.log("query",query);
+        const checkProperties1 = await PropertyModel.find(query);
+
+        const checkProperties = await PropertyModel.aggregate([
+            {$match : query},
+
+            {
+                $lookup: {
+                  from: "businessType",
+                  localField: "businessType", // favorites
+                  foreignField: "id", // property
+                  as: "businessType",
+                },
+              },
+              {
+                $unwind: "$businessType",
+              },
+              {
+                $lookup: {
+                  from: "propertyType",
+                  localField: "propertyType", // favorites
+                  foreignField: "id", // property
+                  as: "propertyType",
+                },
+              },
+              {
+                $unwind: "$propertyType",
+              },
+
+              {
+                $lookup: {
+                  from: "cities",
+                  localField: "city", // favorites
+                  foreignField: "id", // property
+                  as: "city1",
+                },
+              },
+              {
+                $unwind: "$city1",
+              },
+
+                {$project:{"cityName":"$city1.name","zoneName": {$arrayElemAt: [ "$city1.zones", "$zone" ]},"title":1, "city":1,"zone":1, "description":1, "ownerId":1,"propertyType":1, "businessType":1, "mainImage":1, "value":1}}
+
+            ]);
+
         console.log(`Propiedades encontradas`);
         console.log(checkProperties);       
         return responseOk ({mensaje:`mostrando las propiedades disponibles según los filtros aplicados`,checkProperties});       
@@ -76,7 +120,7 @@ const property = async (propertyId) =>{
                 $unwind: "$city1",
               },
 
-
+                {$project:{"cityName":"$city1.name","zoneName": {$arrayElemAt: [ "$city1.zones", "$zone" ]},"title":1, "city":1,"zone":1, "description":1, "ownerId":1,"propertyType":1, "businessType":1, "mainImage":1, "value":1}}
 
             ]);
  
